b32ee69ebf36b7ace916a42670245bed23c0be3c
g70580jh <jinjin.huang-2@student.manchester.ac.uk>
2022-02-23 21:44:42 +0000

    navibar

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <?php
 include("include/common.php");
 ?>
+
+<link rel="stylesheet" href="resources/css/navigation.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -13,6 +16,20 @@ include("include/common.php");
                 echo "<a href='index.php?logout'><button name='logout' type='submit' class='btn btn-outline-primary'>Logout</button></a>";
                 }
                 ?>
+                <!-- <button type="button" class="btn btn-outline-success">Login</button> -->
+                <img src="/resources/images/profile.png"/>
+                <div class="dropdown">
+                    <button class="dropbtn">Username
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="#">Profile</a>
+                        <a href="#">Logout</a>
+                    </div>
+                </div>
+        </div>
+               <!--<h1>Username</h1> -->
+
             </ul>
         </div>
     </div>
