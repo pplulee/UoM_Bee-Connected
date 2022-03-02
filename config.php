a2f2deb4619b1b497fb8aed8d773d2a85b10b430
g70580jh <jinjin.huang-2@student.manchester.ac.uk>
2022-03-02 12:20:03 +0000

    header

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $Sys_config["debug"] = true;
-$Sys_config["mysql_enable"] = false; // If set to FALSE, then website will run without database
+$Sys_config["mysql_enable"] = true; // If set to FALSE, then website will run without database
 //MySQL Connection Information
 $Sys_config["db_host"] = "localhost";
 $Sys_config["db_user"] = "root";
--- include/common.php
+++ include/common.php
@@ -34,6 +34,8 @@ echo'<!DOCTYPE html>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Grand+Hotel&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 </head>
 <body>
 </body>
