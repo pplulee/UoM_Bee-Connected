abd13712391c6f9c5450b226227550cd47dbacaa
g70580jh <jinjin.huang-2@student.manchester.ac.uk>
2022-03-07 18:18:05 +0000

    mainpage

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include("header.php");
-if (!$_SESSION["isLogin"]) {
+/*if (!$_SESSION["isLogin"]) {
     echo "<div class=\"alert alert-warning\" role=\"alert\"><p>You are not logged in yet\n</p></div>";
     echo "<a href=\"login.php\"><button type=\"button\" class=\"btn btn-primary\">Go to Login</button></a>";
     exit;
@@ -9,4 +9,12 @@ if (!$_SESSION["isLogin"]) {
 }
 if (isset($_GET["logout"])) {
     logout();
-}
+}*/
+?>
+<link rel="stylesheet" href="resources/css/index.css">
+<div class="category">
+
+</div>
+<div class="trending">
+
+</div>
