fc9d62859b9efa90a9ca0ef7692b8c76be12dca6
g70580jh <jinjin.huang-2@student.manchester.ac.uk>
2022-03-23 17:41:11 +0000

    header

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $Sys_config["debug"] = true;
 $Sys_config["mysql_enable"] = true; // If set to FALSE, then website will run without database
 //MySQL Connection Information
-$Sys_config["db_host"] = "localhost";
-$Sys_config["db_user"] = "root";
-$Sys_config["db_password"] = "123456";
-$Sys_config["db_database"] = "webtest";
+$Sys_config["db_host"] = "ss2.mkid.top";
+$Sys_config["db_user"] = "groupr3";
+$Sys_config["db_password"] = "groupR3";
+$Sys_config["db_database"] = "groupr3";
