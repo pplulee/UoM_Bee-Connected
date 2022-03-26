2570c28f23a2ee63796d5626ce6a69219b8e1fdd
022by <46434255+pplulee@users.noreply.github.com>
2022-03-26 23:01:07 +0000

    fix post.php

--- post.php
+++ post.php
@@ -1,16 +1,5 @@
 <?php
 include("header.php");
-/*if (!$_SESSION["isLogin"]) {
-    echo "<div class=\"alert alert-warning\" role=\"alert\"><p>You are not logged in yet\n</p></div>";
-    echo "<a href=\"login.php\"><button type=\"button\" class=\"btn btn-primary\">Go to Login</button></a>";
-    exit;
-} else {
-    echo "If you see this message, it means you are logged in\n";
-}
-}*/
-if (isset($_GET["logout"])) {
-    logout();
-}
 ?>
     <head>
         <script src="resources/js/read_more.js"></script>
