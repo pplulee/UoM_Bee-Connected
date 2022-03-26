5dac4567dcb4c86e94c3da12141972bbf2a9bb92
pplulee <46434255+pplulee@users.noreply.github.com>
2022-03-26 22:13:11 +0000

    Revert "Merge branch 'Pavel_post' into 'master'"
    This reverts merge request !7

--- index.php
+++ index.php
@@ -57,10 +57,10 @@ if (isset($_GET["logout"])) {
                                         <div class = 'img_name_report' id = 'img_name_report'>
                                             <img src='$userpic'>
                                             <h1>$username</h1>
-                                            <a class='report'>
+                                            <button class='report'>
                                                 <p class='text_1'>!</p>
                                                 <p class='text_2'>Report</p>
-                                            </a>";
+                                            </button>";
                             if ($_SESSION["isLogin"] and isauthor($_SESSION["userid"], $pid)) {
                                 echo "
                                     <a href='index.php?action=delete&pid=$pid' class = 'delete_post' >
@@ -100,11 +100,11 @@ if (isset($_GET["logout"])) {
                                     while ($row = mysqli_fetch_assoc($result)) {
                                         echo "<option value='{$row['name']}'>{$row['name']}</option>";
                                     }
-                                }s
+                                }
                                 ?>
                             </select>
                             <button class="send_post" name="send_post" type=submit><i
-                 s                      class="fa-solid fa-paper-plane"></i></button>
+                                        class="fa-solid fa-paper-plane"></i></button>
 
                             <input id="browse" type="file" name="post_pic" hidden>
                             <input class="btn btn-primary" type="submit" name="upload" value="UPDATE" hidden>
